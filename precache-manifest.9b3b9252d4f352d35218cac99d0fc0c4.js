6f45f852bb81b1b7ca42a7a1179d4c1491bff70c
candelaDoloresDengra <candela@magoya.com>
2022-05-02 21:04:11 -0300

    Updates

---
+++ precache-manifest.9b3b9252d4f352d35218cac99d0fc0c4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96dc72b03d693853e558fe5a8e4d9470",
+    "url": "/baratmed/index.html"
+  },
+  {
+    "revision": "c093113f0814ce0426c4",
+    "url": "/baratmed/static/css/2.66f9e08a.chunk.css"
+  },
+  {
+    "revision": "c6d13d3e13e1ed853488",
+    "url": "/baratmed/static/css/main.f34b84bc.chunk.css"
+  },
+  {
+    "revision": "c093113f0814ce0426c4",
+    "url": "/baratmed/static/js/2.b271296e.chunk.js"
+  },
+  {
+    "revision": "564a8286d91b6de9c0fef56cd10b7ba3",
+    "url": "/baratmed/static/js/2.b271296e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "859b85f5c5a7ed59e1fc",
+    "url": "/baratmed/static/js/3.b5084446.chunk.js"
+  },
+  {
+    "revision": "c6d13d3e13e1ed853488",
+    "url": "/baratmed/static/js/main.8804e8b5.chunk.js"
+  },
+  {
+    "revision": "60ffbb7e8ca2f5d3223f",
+    "url": "/baratmed/static/js/runtime-main.f743fd67.js"
+  },
+  {
+    "revision": "40e885e3fbb009c74b44b6d0b2516a58",
+    "url": "/baratmed/static/media/logo.40e885e3.png"
+  }
+]);
